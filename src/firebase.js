9eb1628c9a6fd2d00fe6de9d01678b4adaa75fa7
Jose Guerra <jrguerra.180490@gmail.com>
2020-11-05 21:19:07 -0300

    Nasa demo

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC6rboySAvnKNkGhIWyDEhtSgi-HTz9LRY",
+    authDomain: "talentodigital-392ad.firebaseapp.com",
+    databaseURL: "https://talentodigital-392ad.firebaseio.com",
+    projectId: "talentodigital-392ad",
+    storageBucket: "talentodigital-392ad.appspot.com",
+    messagingSenderId: "825979126156",
+    appId: "1:825979126156:web:402a1dfb5cfe8322e796a9"
+  };
+
+export default firebaseConfig;
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,32 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
+const NASA_KEY="H4ViujClUioe4GXXNiKAcjkikRDHAtUowmiCBD5f";
+
 export default new Vuex.Store({
   state: {
+    apod: ''
   },
   mutations: {
-  },
+    GET_APOD(state, apod){
+      state.apod = apod
+    }
+    },
   actions: {
+    getApod({commit}){
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}`)
+      .then(response => {
+        console.log(response.data)
+          commit("GET_APOD", response.data)
+      }).catch(() => {
+        commit("GET_APOD", {
+          url:"https://apod.nasa.gov/apod/image/2010/NGC5643_HubbleZamani_960.jpg"
+        })
+      })
+    }
   },
   modules: {
   }
